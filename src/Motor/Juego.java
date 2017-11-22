64a119a6401c53f239b03f052dfee8410500e6b6
luis.uzcategui <luis.uzcategui@C10-EQUIPO19.labcomp.unet.edu.ve>
2017-11-22 00:13:14 -0400

    Merge origin/master
    Conflicts:
    	.gitignore
    	src/Motor/Juego.java
    	src/Motor/Lienzo.java
    	src/Personajes/Enemigo.java
    	src/Principal/Mapa.java

--- src/Motor/Juego.java
+++ src/Motor/Juego.java
@@ -21,12 +21,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Juego extends JFrame{
-    Panel r;
+    Panel p;
     Random num, n;
     Timer t1, tiempo;
     Lienzo pantalla;
@@ -43,7 +44,7 @@ public class Juego extends JFrame{
     Image logo;
     
 
-    public Juego(){        
+    public Juego(){
         sonido=new Sfx();
         pantalla = new Lienzo();
         tiempo = new Timer(1000,time);        
@@ -96,15 +97,11 @@ public class Juego extends JFrame{
             pantalla.rio.puntaje=Integer.toString(pantalla.jugador.puntaje);
             pantalla.gasoil.y += pantalla.mov;
             pantalla.enemigo1.y += pantalla.mov;
-<<<<<<< HEAD
             pantalla.enemigo1.x += pantalla.mov*b1;
             pantalla.enemigo2.y += pantalla.mov;
             pantalla.enemigo2.x += pantalla.mov+25*b2;
             pantalla.enemigo3.y += pantalla.mov+30;
             pantalla.enemigo3.x += pantalla.mov*b3;
-=======
-            pantalla.enemigo1.x += pantalla.mov*b ;
->>>>>>> origin/master
             pantalla.bala.y -= 30;
             pantalla.rio.y+=pantalla.mov;
             pantalla.bum.y+=pantalla.mov;
@@ -171,11 +168,7 @@ public class Juego extends JFrame{
             if( pantalla.jugador.area().intersects(pantalla.gasoil.area())){
                 pantalla.gasoil.y = 1100;
                 pantalla.jugador.cant_gasoil = 100;
-<<<<<<< HEAD
                 pantalla.jugador.puntaje += 10;
-=======
-                pantalla.jugador.puntaje += 50;
->>>>>>> origin/master
                       try {
                     sonido.sonido(sonido.gas);
                 } catch (IOException ex) {}
@@ -236,11 +229,7 @@ public class Juego extends JFrame{
             }  
             
             if (pantalla.enemigo1.y > 850){   
-<<<<<<< HEAD
                 pantalla.enemigo1.x = num.nextInt(800);
-=======
-                pantalla.enemigo1.x = num.nextInt(400)+250;
->>>>>>> origin/master
                 pantalla.enemigo1.y = -10;
             }
             for (int i = 0; i < 350; i++) {
@@ -393,7 +382,6 @@ public class Juego extends JFrame{
 
     ActionListener time = new ActionListener(){        
         public void actionPerformed(ActionEvent e) { 
-<<<<<<< HEAD
             auxs++;
             if(auxs==60){
                 auxs=0;
@@ -401,13 +389,6 @@ public class Juego extends JFrame{
             }
             pantalla.rio.tiempo=("0"+auxm+":"+auxs);
             if(auxs %5 == 0){ // aparece gasolina cada 5 segundos
-=======
-            aux++;
-            String aux2 = Integer.toString(aux);            
-            System.out.println("00:"+aux2);
-            pantalla.rio.tiempo=("00:"+aux2);
-            if(aux %5 == 0){ // aparece gasolina cada 5 segundos
->>>>>>> origin/master
                 int band=0;
                 for (int i = 0; i < 350; i++) {
                     pantalla.gasoil.x = num.nextInt(400)+250;
@@ -431,15 +412,10 @@ public class Juego extends JFrame{
                 }
   
             }
-<<<<<<< HEAD
  // disminuye gasolina cada 3 seguntos
                 pantalla.jugador.cant_gasoil -=14;
                 pantalla.rio.bargasx-=54;
 
-=======
-                pantalla.jugador.cant_gasoil -=14;
-                pantalla.rio.bargasx-=54;
->>>>>>> origin/master
         }
     }; 
  
@@ -479,4 +455,4 @@ public class Juego extends JFrame{
         
     }
 
-}
+}    
